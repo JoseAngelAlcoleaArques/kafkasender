d5ad1a3025dded3572e4bb09deaafeffba9acc4f
JaArques <jaalcolea@ucam.edu>
2023-07-18 10:14:46 +0200

    Cambios a JSON

--- src/main/groovy/edu/ucam/kafkasender/config/KafkaProducerConfig.java
+++ src/main/groovy/edu/ucam/kafkasender/config/KafkaProducerConfig.java
@@ -1,15 +1,14 @@
 package edu.ucam.kafkasender.config;
 
-import edu.ucam.kafkasender.entity.Mensaje;
 import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
-import org.springframework.kafka.support.serializer.JsonSerializer;
+import org.springframework.kafka.support.converter.StringJsonMessageConverter;
+
 
 import java.util.HashMap;
 import java.util.Map;
@@ -21,18 +20,18 @@ public class KafkaProducerConfig {
     private String bootstrapAddress;
 
     @Bean
-    public ProducerFactory<String, Mensaje> producerFactory() {
+    public ProducerFactory<String, String> producerFactory() {
         Map<String, Object> configProps = new HashMap<>();
-
         configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
-        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, org.apache.kafka.common.serialization.StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, org.apache.kafka.common.serialization.StringSerializer.class);
         return new DefaultKafkaProducerFactory<>(configProps);
     }
 
     @Bean
-    public KafkaTemplate<String, Mensaje> kafkaTemplate(){
-        return new KafkaTemplate<>(producerFactory());
+    public KafkaTemplate<String, String> kafkaTemplate() {
+        KafkaTemplate<String, String> kafkaTemplate = new KafkaTemplate<>(producerFactory());
+        kafkaTemplate.setMessageConverter(new StringJsonMessageConverter());
+        return kafkaTemplate;
     }
 }
--- src/main/groovy/edu/ucam/kafkasender/services/MensajeService.java
+++ src/main/groovy/edu/ucam/kafkasender/services/MensajeService.java
@@ -1,5 +1,7 @@
 package edu.ucam.kafkasender.services;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import edu.ucam.kafkasender.entity.Mensaje;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
@@ -7,15 +9,25 @@ import org.springframework.stereotype.Service;
 @Service
 public class MensajeService {
 
-    private final KafkaTemplate<String, Mensaje> producer;
+    private String topicMensaje = "test_topic";
+    private final KafkaTemplate<String, String> producer;
 
-    public MensajeService(KafkaTemplate<String, Mensaje> producer) {
+    public MensajeService(KafkaTemplate<String, String> producer) {
         this.producer = producer;
     }
 
-    private String topicMensaje = "test_topic";
-
     public void publish(Mensaje mensaje){
-        this.producer.send(topicMensaje, mensaje);
+        String jsonString = convertToJsonString(mensaje);
+        this.producer.send(topicMensaje, jsonString);
+    }
+
+    private String convertToJsonString(Mensaje mensaje) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            return objectMapper.writeValueAsString(mensaje);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
